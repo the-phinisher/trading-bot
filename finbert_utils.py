c09b34b4726e5237e7704ac69d4b89ee1c74e2c2
Gandhar Vabale <99757636+the-phinisher@users.noreply.github.com>
2024-06-30 13:24:30 +0530

    Add better tests for finbert_utils

--- finbert_utils.py
+++ finbert_utils.py
@@ -1,7 +1,17 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch
-from typing import Tuple 
-device = "cuda:0" if torch.cuda.is_available() else "cpu"
+from typing import Tuple
+
+device_prority = [
+    ("cuda", torch.cuda.is_available()),
+    ("mps", torch.backends.mps.is_available()),
+]
+
+device = "cpu"
+for device_name, is_available in device_prority:
+    if is_available:
+        device = device_name
+    
 
 tokenizer = AutoTokenizer.from_pretrained("ProsusAI/finbert")
 model = AutoModelForSequenceClassification.from_pretrained("ProsusAI/finbert").to(device)
@@ -23,6 +33,24 @@ def estimate_sentiment(news):
 
 
 if __name__ == "__main__":
+    print("device:", device)
+    # mix of news
     tensor, sentiment = estimate_sentiment(['markets responded negatively to the news!','traders were happy!', 'the stock remained almost the same', 'mathematical equations solved'])
     print(tensor, sentiment)
-    print(torch.cuda.is_available())
+    assert sentiment == 'neutral'
+    # positive news
+    tensor, sentiment = estimate_sentiment(['traders were happy!', 'stock grew significantly'])
+    print(tensor, sentiment)
+    assert sentiment == 'positive'
+    # negative news
+    tensor, sentiment = estimate_sentiment(['markets responded negatively to the news!'])
+    print(tensor, sentiment)
+    assert sentiment == 'negative'
+    # neutral news
+    tensor, sentiment = estimate_sentiment(['the stock remained almost the same'])
+    print(tensor, sentiment)
+    assert sentiment == 'neutral'
+    # irrelevant news
+    tensor, sentiment = estimate_sentiment(['mathematical equations solved'])
+    print(tensor, sentiment)
+    assert sentiment == 'neutral'
