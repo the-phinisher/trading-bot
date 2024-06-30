76641183d6fe3fc36808105c9ec3614928add70c
Gandhar Vabale <99757636+the-phinisher@users.noreply.github.com>
2024-06-30 16:34:38 +0530

    Add black formatter
     - Auto formatting code for future

--- finbert_utils.py
+++ finbert_utils.py
@@ -11,11 +11,14 @@ device = "cpu"
 for device_name, is_available in device_prority:
     if is_available:
         device = device_name
-    
+
 
 tokenizer = AutoTokenizer.from_pretrained("ProsusAI/finbert")
-model = AutoModelForSequenceClassification.from_pretrained("ProsusAI/finbert").to(device)
+model = AutoModelForSequenceClassification.from_pretrained("ProsusAI/finbert").to(
+    device
+)
 labels = ["positive", "negative", "neutral"]
+
 
 def estimate_sentiment(news):
     if news:
@@ -35,22 +38,33 @@ def estimate_sentiment(news):
 if __name__ == "__main__":
     print("device:", device)
     # mix of news
-    tensor, sentiment = estimate_sentiment(['markets responded negatively to the news!','traders were happy!', 'the stock remained almost the same', 'mathematical equations solved'])
+    tensor, sentiment = estimate_sentiment(
+        [
+            "markets responded negatively to the news!",
+            "traders were happy!",
+            "the stock remained almost the same",
+            "mathematical equations solved",
+        ]
+    )
     print(tensor, sentiment)
-    assert sentiment == 'neutral'
+    assert sentiment == "neutral"
     # positive news
-    tensor, sentiment = estimate_sentiment(['traders were happy!', 'stock grew significantly'])
+    tensor, sentiment = estimate_sentiment(
+        ["traders were happy!", "stock grew significantly"]
+    )
     print(tensor, sentiment)
-    assert sentiment == 'positive'
+    assert sentiment == "positive"
     # negative news
-    tensor, sentiment = estimate_sentiment(['markets responded negatively to the news!'])
+    tensor, sentiment = estimate_sentiment(
+        ["markets responded negatively to the news!"]
+    )
     print(tensor, sentiment)
-    assert sentiment == 'negative'
+    assert sentiment == "negative"
     # neutral news
-    tensor, sentiment = estimate_sentiment(['the stock remained almost the same'])
+    tensor, sentiment = estimate_sentiment(["the stock remained almost the same"])
     print(tensor, sentiment)
-    assert sentiment == 'neutral'
+    assert sentiment == "neutral"
     # irrelevant news
-    tensor, sentiment = estimate_sentiment(['mathematical equations solved'])
+    tensor, sentiment = estimate_sentiment(["mathematical equations solved"])
     print(tensor, sentiment)
-    assert sentiment == 'neutral'
+    assert sentiment == "neutral"
